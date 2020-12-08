d690c89e0e7a4f7fe5bcfabf7058a4e1dd46ecdb
Gonzalo Berhouet <gonzaloberhouet@MA333.fritz.box>
2020-12-08 14:08:23 +0000

    json update

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
-
+//app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+app.get("/", (req, resp) => {
+  resp.json({ hello: "hello Kiril" });
+});
 app.listen(4000, () => {
   console.log("Listening on port 4000");
 });
