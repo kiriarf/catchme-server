c9501b8892cc8dbf281d710ed47bda6e5ccec122
Gonzalo Berhouet <gonzaloberhouet@MA333.fritz.box>
2020-12-08 16:38:14 +0000

    updateUser resolvers and schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,86 +1,91 @@
 const resolvers = {
   Query: {
-      async user (root, { id }, { models }) {
-        return models.User.findByPk(id)
-      },
-      async users (root, args, { models }) {
-        return models.User.findAll()
-      },
-      async race (root, { id }, { models }) {
-        return models.Race.findByPk(id)
-      },
-      async races (root, args, { models }) {
-        return models.Race.findAll()
-      },
-      async location (root, { id }, { models }) {
-        return models.Location.findByPk(id)
-      },
-      async score (root, { id }, { models }) {
-        return models.Score.findByPk(id)
-      },
+    async user(root, { id }, { models }) {
+      return models.User.findByPk(id);
     },
+    async users(root, args, { models }) {
+      return models.User.findAll();
+    },
+    async race(root, { id }, { models }) {
+      return models.Race.findByPk(id);
+    },
+    async races(root, args, { models }) {
+      return models.Race.findAll();
+    },
+    async location(root, { id }, { models }) {
+      return models.Location.findByPk(id);
+    },
+    async score(root, { id }, { models }) {
+      return models.Score.findByPk(id);
+    },
+  },
 
   Mutation: {
-    async createRace (root, { distance, startTime, endTime }, { models }) {
-        return models.Race.create({
-          distance, 
-          startTime,
-          endTime
-        })
-    },
-    async createUser (root, { username, position, raceId }, { models }) {
-      return models.User.create({
-          username,
-          position,
-          raceId
-      })
-    },
-    async createLocation (root, { startLat, startLong, endLat, endLong, distance, userId }, { models }) {
-      return models.Location.create({
-        startLat, 
-        startLong, 
-        endLat, 
-        endLong, 
+    async createRace(root, { distance, startTime, endTime }, { models }) {
+      return models.Race.create({
         distance,
-        userId
-      })
+        startTime,
+        endTime,
+      });
     },
-    async createScore (root, { time, userId }, { models }) {
-      return models.Score.create({ time, userId })
+    async createUser(root, { username, position, raceId }, { models }) {
+      return models.User.create({
+        username,
+        position,
+        raceId,
+      });
+    },
+    async createLocation(
+      root,
+      { startLat, startLong, endLat, endLong, distance, userId },
+      { models }
+    ) {
+      return models.Location.create({
+        startLat,
+        startLong,
+        endLat,
+        endLong,
+        distance,
+        userId,
+      });
+    },
+    async createScore(root, { time, userId }, { models }) {
+      return models.Score.create({ time, userId });
+    },
+    async updateUser(root, { id, position }, { models }) {
+      return models.User.update({ position: position }, { where: { id: id } });
     },
   },
 
   Race: {
-    async users (race) {
-      return race.getUsers()
-    }
+    async users(race) {
+      return race.getUsers();
+    },
   },
 
   User: {
-    async race (user) {
-      return user.getRace()
+    async race(user) {
+      return user.getRace();
     },
-    async location (user) {
-      return user.getLocation()
+    async location(user) {
+      return user.getLocation();
     },
-    async score (user) {
-      return user.getScore()
-    }
+    async score(user) {
+      return user.getScore();
+    },
   },
 
   Location: {
-    async user (location) {
-      return location.getUser()
-    }
+    async user(location) {
+      return location.getUser();
+    },
   },
 
   Score: {
-    async user (score) {
-      return score.getUser()
-    }
-  }
+    async user(score) {
+      return score.getUser();
+    },
+  },
+};
 
-
-}
-
-module.exports = resolvers
+module.exports = resolvers;
--- src/schema.js
+++ src/schema.js
@@ -1,61 +1,62 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-    type Race {
-      id: Int!
-      distance: Int!
-      startTime: Int
-      endTime: Int
-      users: [User]
-      }
+  type Race {
+    id: Int!
+    distance: Int!
+    startTime: Int
+    endTime: Int
+    users: [User]
+  }
 
-    type User {
-      id: Int!
-      username: String!
-      position: Int
-      race: Race!
-      location: Location
-      score: Score
-    }
+  type User {
+    id: Int!
+    username: String!
+    position: Int
+    race: Race!
+    location: Location
+    score: Score
+  }
 
-    type Location {
-      id: Int!
+  type Location {
+    id: Int!
+    startLat: Float!
+    startLong: Float!
+    endLat: Float!
+    endLong: Float!
+    distance: Float!
+    user: User!
+  }
+
+  type Score {
+    id: Int!
+    time: Int!
+    user: User!
+  }
+
+  type Query {
+    user(id: Int!): User!
+    users: [User]
+    race(id: Int!): Race!
+    location: Location
+    score: Score
+    races: [Race]
+  }
+
+  type Mutation {
+    createRace(distance: Int!, startTime: Int, endTime: Int): Race!
+    createUser(username: String!, position: Int, raceId: Int!): User!
+    createLocation(
       startLat: Float!
       startLong: Float!
       endLat: Float!
       endLong: Float!
       distance: Float!
-      user: User!
-    }
+      userId: Int!
+    ): Location!
+    createScore(time: Int!, userId: Int!): Score!
+    updateUser(id: Int!, position: Int!): User!
+  }
+`;
 
-    type Score {
-      id: Int!
-      time: Int!
-      user: User!
-    }
-
-    type Query {
-        user(id: Int!): User!
-        users: [User]
-        race(id: Int!): Race!
-        location: Location
-        score: Score
-        races: [Race]
-    }
-
-    type Mutation {
-        createRace(distance: Int!, startTime: Int, endTime: Int): Race!
-        createUser(username: String!, position: Int, raceId: Int!): User!
-        createLocation(
-          startLat: Float!, 
-          startLong: Float!, 
-          endLat: Float!, 
-          endLong: Float!, 
-          distance: Float!,
-          userId: Int!): Location!
-        createScore(time: Int!, userId: Int!): Score!
-
-    }
-`
-
-module.exports = typeDefs
+module.exports = typeDefs;
