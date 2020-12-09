16a178e4e2e120ad6e55ad3f8de8cc442fce09f9
Gonzalo Berhouet <gonzaloberhouet@MA333.fritz.box>
2020-12-09 14:01:36 +0000

    update config.json

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: { models },
+  introspection: true,
+  playground: true,
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
