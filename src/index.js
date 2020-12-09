c97974de32152ec134232d9878199697fbe48b87
Gonzalo Berhouet <gonzaloberhouet@MA333.fritz.box>
2020-12-09 13:55:49 +0000

    update config.json

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const models = require('../models');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const models = require("../models");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: { models }
-})
+  context: { models },
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
-
 
 // server.listen(
 //   {
