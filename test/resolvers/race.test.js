2d38217535dca0aba8427cf2b852b6098ffc9bdb
Gonzalo Berhouet <gonzaloberhouet@MA333.fritz.box>
2020-12-10 08:48:53 +0000

    race test fixed

--- test/resolvers/race.test.js
+++ test/resolvers/race.test.js
@@ -1,9 +1,9 @@
-const gql = require('graphql-tag');
+const gql = require("graphql-tag");
 const { ApolloServer } = require("apollo-server");
 const resolvers = require("../../src/resolvers/race.js");
 const typeDefs = require("../../src/schema");
 const models = require("../../models");
-const { createTestClient } = require('apollo-server-testing');
+const { createTestClient } = require("apollo-server-testing");
 const db = require("../../models/index");
 
 const CREATE_RACE = gql`
@@ -14,31 +14,52 @@ const CREATE_RACE = gql`
     }
   }
 `;
-describe('Race resolvers', () => {
+const QUERY_RACE = gql`
+  query race($id: Int!) {
+    race(id: $id) {
+      id
+      distance
+    }
+  }
+`;
+describe("Race resolvers", () => {
+  let server;
   beforeAll(async () => {
-    await db.sequelize.sync({ force: true })
-  })
+    await db.sequelize.sync({ force: true });
+  });
 
-  // afterAll(() => {
-  //   // Closing the DB connection allows Jest to exit successfully.
-  //   // mongoose.connection.close()
-  //   // await process.exit()
-  //   // return db.close()
-  //   // await server.close()
-  // })
-
-  it('fetches single race', async () => {
-    const server = new ApolloServer({
+  beforeEach(async () => {
+    server = new ApolloServer({
       typeDefs,
       resolvers,
       context: { models },
     });
-  
+  });
+
+  afterAll(async () => {
+    await db.sequelize.close();
+  });
+  afterEach(async () => {
+    await server.stop();
+  });
+
+  it("fetches single race", async () => {
     const { query, mutate } = createTestClient(server);
-  
-    const res = await mutate({ mutation: CREATE_RACE, variables: { distance: 1000 } });
-    // res.data.createRace.id = 1;
+    const res = await mutate({
+      mutation: CREATE_RACE,
+      variables: { distance: 1000 },
+    });
     expect(res).toMatchSnapshot();
   });
-  
+
+  it("fetches single race", async () => {
+    const { query, mutate } = createTestClient(server);
+    const res = await query({
+      query: QUERY_RACE,
+      variables: { id: 1 },
+    });
+    expect(res).toMatchSnapshot();
+    expect(res.data).toEqual({ race: { distance: 1000, id: 1 } });
+    expect(res.error).toEqual(undefined);
+  });
 });
